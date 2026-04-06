e26dc8fa381d30f1e473c11993539f933901b44e
ndokaman <160651692+ndokaman@users.noreply.github.com>
2026-04-06 13:39:41 -0400

    yo

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,24 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Henry"; // STUDENT_TODO_1A: Change name
+
     // STUDENT_TODO_1B: Add a nickname or title for the barista
+
+    private static String nickname = "CoffeeMan";
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+
+        int newCup = cups + 1;
+        System.out.print("Added 1 to cup");
+
         // STUDENT_TODO_2B: Add a second brew-related improvement
+
+        String sugar = "sugar";
+        String addedSugger = drink + sugar;
+
     }
 
     public static void printSummary() {
@@ -20,3 +31,5 @@ public class CommitCafe {
         printSummary();
     }
 }
+
+// sorry for the bad code haha
